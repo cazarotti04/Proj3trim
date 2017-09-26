f5447c847bc33f1f5507007f51ef1ff33d24e344
Cazarotti04 <cazarotti04@gmail.com>
2017-09-26 11:33:37 -0300

    Started project

---
+++ Assets/Scripts/MainMenuUICtlr.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUICtlr : MonoBehaviour {
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
+	}
+	
+}
