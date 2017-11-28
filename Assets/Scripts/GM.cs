96acccd0fc006699f8e35beb18188e30d913e802
cazarotti04 <cazarotti04@gmail.com>
2017-11-27 22:55:33 -0200

    Acrescentando a função dos botões

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GM : MonoBehaviour {
 
@@ -49,6 +50,22 @@ public class GM : MonoBehaviour {
 		}
 		UpdateTimer(); 
 		DisplayHudData();
+	}
+
+	public void restartLevel(){
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void ExitToMainMenu(){
+		LoadScene("MainMenu");
+	}
+
+	public void closeApp(){
+		Application.Quit();
+	}
+
+	public void LoadScene(string sceneName){
+		SceneManager.LoadScene(sceneName);
 	}
 
 	void UpdateTimer(){
